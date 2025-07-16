4bf9784fbe9b2b6ea804c49e152b65ce100656eb
KanhaiyaBagul <bagulkanhaiya60@gmail.com>
2025-07-16 16:51:05 +0530

    Time: 0 ms (100.00%) | Memory: 96.2 MB (40.00%) - LeetSync

--- 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.cpp
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.cpp
@@ -1,55 +1,26 @@
 class Solution {
 public:
     int maximumLength(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> sub1;
-        int maxLength = INT_MIN;
+        
+        int evenCount = 0;
+        int oddCount = 0;
+        int alternat = 0;
 
-        for(int i = 0; i < n; i++){
-            if(nums[i] % 2 == 0){
-                sub1.push_back(nums[i]);
+        for (int num : nums){
+            if(num % 2 == 0) evenCount++;
+            else oddCount++;
+        }
+
+        int expectedParity = nums[0] % 2;
+        for (int num : nums){
+            if(num % 2 == expectedParity){
+                alternat++;
+                expectedParity ^= 1;
             }
         }
 
-        maxLength = max(maxLength,(int)sub1.size()); //We use the int because the sub1.size is of "size_t" in the max fun we require the same type of type  
+        return max({evenCount,oddCount,alternat});
 
-
-        sub1.clear();
-
-        for(int i = 0; i < n; i++){
-            if(nums[i] % 2 == 1){
-                sub1.push_back(nums[i]);
-            }
-        }
-
-        maxLength = max(maxLength,(int)sub1.size());
-        sub1.clear();
-
-
-        int i = 0;
-        bool flag = 0;
-
-        if(nums[0] % 2 == 0){
-            flag = 0;
-
-        }
-
-        else{
-            flag = 1;
-        }
-
-        while(i < n){
-            if(nums[i] % 2 == flag){
-                sub1.push_back(nums[i]);
-                flag = !flag;
-            }
-           
-
-            i++;
-        }
-
-       maxLength = max(maxLength,(int)sub1.size());
-        return maxLength;
 
         
     }
