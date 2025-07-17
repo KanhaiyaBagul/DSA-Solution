c628d444e32703d8df102112eb4e95ef436c5a82
KanhaiyaBagul <bagulkanhaiya60@gmail.com>
2025-07-17 13:22:40 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (8.46%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
+
+// Helper function to solve the linear version of the problem
+// i.e when houses are not arranged in a cirle
     int robHelper(vector<int> nums) {
         int n = nums.size();
+
+        // Base case : no house to rob
         if (n == 0)
             return 0;
+
+        //Base case : only one house to rob
         if (n == 1)
             return nums[0];
 
-        vector<int> dp(n);
-        dp[0] = nums[0];
-        dp[1] = max(dp[0], nums[1]);
+        //DP array to store max loot up to each house    
 
+        vector<int> dp(n);
+        dp[0] = nums[0]; // rob the first house
+        dp[1] = max(dp[0], nums[1]);//max of robbing 1st or 2nd house
+
+
+        // Fill dp[i] using the choice  : rob current of skip it
         for (int i = 2; i < n; i++) {
+            //Either rob current house + dp[i - 2] or skip current 
             dp[i] = max(dp[i - 2] + nums[i], dp[i - 1]);
         }
-
+        //The last element contains the answer for this linear case
         return dp[n - 1];
     }
     int rob(vector<int>& nums) {
@@ -34,6 +46,7 @@ public:
             skipFirstHouse[i] = nums[i + 1];
         }
 
+        //Call helper on both cases and return the maximum loot possible
         int lootSkipLastHouse = robHelper(skipLastHouse);
         int lootSkipFirstHouse = robHelper(skipFirstHouse);
 
