be18f6b4262939d310d8279b7e32009a16602e05
KanhaiyaBagul <bagulkanhaiya60@gmail.com>
2025-07-19 22:26:52 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (93.62%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -10,7 +10,7 @@ public:
             if(sum == target){
                 result.push_back(start + 1);
                 result.push_back(end + 1);
-                
+                break;
                 return result;
             }else if(sum < target){
                 start++;
