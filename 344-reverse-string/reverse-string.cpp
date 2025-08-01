c7b99dce1e51859ca02ddb49a92a77c0f9c93c32
KanhaiyaBagul <bagulkanhaiya60@gmail.com>
2025-08-01 15:30:38 +0530

    Time: 0 ms (100.00%) | Memory: 27.3 MB (51.19%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -5,7 +5,9 @@ public:
         int i = 0; 
         int j = n  - 1;
         while(i <= j){
-            swap(s[i],s[j]);
+            char ch = s[i];
+            s[i] = s[j];
+            s[j] = ch;
             i++;
             j--;
         }
