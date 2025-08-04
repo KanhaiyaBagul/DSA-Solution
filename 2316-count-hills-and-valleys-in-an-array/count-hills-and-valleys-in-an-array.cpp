152314b23bca0797c5cfe36392bb55dcb5614a3e
KanhaiyaBagul <bagulkanhaiya60@gmail.com>
2025-08-04 16:29:42 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (45.06%) - LeetSync

--- 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.cpp
+++ 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int countHillValley(vector<int>& nums) {
-        
         int m = nums.size();
         int count = 0;
         if(m < 3) return 0;//if the array is less than 3 it can't form any hill or a vally
         
-        //We have to remove the duplicates
+        //We have to remove the duplicates nums = [2,4,1,1,6,5]
         vector<int> noDupli;
         noDupli.push_back(nums[0]);
         for(int i = 1; i < m; i++){
@@ -15,21 +14,18 @@ public:
             }
         }
         int n = noDupli.size();
-
         //Count the hills 
         for(int i = 1; i < n - 1 ; i++){
             if(noDupli[i] > noDupli[i - 1] && noDupli[i] > noDupli[i + 1]){
                 count ++;
             }
         }
-
         //Count the vally;
         for(int i = 1; i < n - 1; i++){
             if(noDupli[i] < noDupli[i - 1] && noDupli[i] < noDupli[i + 1]){
                 count ++;
             }
         }
-
         return count;
         
     }
