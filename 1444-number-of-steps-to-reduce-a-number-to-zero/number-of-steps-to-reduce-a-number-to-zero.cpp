6bbf8d72911c13d9bd77077d8c6b8aee342d1e69
KanhaiyaBagul <bagulkanhaiya60@gmail.com>
2025-08-13 06:53:55 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (43.60%) - LeetSync

--- 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.cpp
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -4,10 +4,11 @@ public:
         int steps = 0;
 
         while(num > 0){
-            if(num % 2 == 0){
-                num = num  / 2;
-            }else
-            num = num - 1 ;
+            if((num & 1 ) == 0){
+                num = num / 2;
+            }else {
+                num = num - 1;
+            }
             steps++;
         }
         
