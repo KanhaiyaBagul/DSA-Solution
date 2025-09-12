8165e8691812152d4f1bc8325d1e09b99abfe691
KanhaiyaBagul <bagulkanhaiya60@gmail.com>
2025-09-12 08:57:04 +0530

    Time: 2 ms (89.18%) | Memory: 19 MB (53.73%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
@@ -4,6 +4,11 @@ public:
     bool isVowel(char ch){
         return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');
     }
+
+    //If the string has 0 vowels → Alice loses (false).
+    //If the string has ≥1 vowel → Alice wins (true)
+
+
     bool doesAliceWin(string s) {
         if(s.empty()) return true;
         int count = 0;
@@ -13,8 +18,6 @@ public:
             }
         }
 
-
-       
        return false; 
         
 
