e6d01707c8e6eb0d8c0431269a715d72a112baa8
KanhaiyaBagul <bagulkanhaiya60@gmail.com>
2025-09-12 08:57:41 +0530

    Time: 3 ms (87.13%) | Memory: 19 MB (53.73%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
-
-    bool isVowel(char ch){
-        return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');
-    }
-
     //If the string has 0 vowels → Alice loses (false).
     //If the string has ≥1 vowel → Alice wins (true)
-
-
     bool doesAliceWin(string s) {
         if(s.empty()) return true;
         int count = 0;
         for(char ch : s){
-            if(isVowel(ch)){
+            if((ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')){
                 return true;
             }
         }
