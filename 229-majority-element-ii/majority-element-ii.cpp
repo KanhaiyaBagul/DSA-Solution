83ba4a75cda54e77d7f4fd8a3e3b5aa5ab37fe90
KanhaiyaBagul <bagulkanhaiya60@gmail.com>
2025-09-18 23:23:27 +0530

    Time: 11 ms (23.16%) | Memory: 27 MB (10.21%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -4,11 +4,13 @@ public:
         int n = nums.size();
         vector<int> result;
 
-        unordered_map<int , int> map;
+        //first we calculate the req
+        map<int , int> map;
         for(int i = 0;  i < n; i++){
             map[nums[i]]++;
         }
 
+        //find the freq greater that n / 3
         for(auto pair : map){
             if(pair.second > n / 3){
                 result.push_back(pair.first);
