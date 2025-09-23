86809b8eb5ba8fd6a25939878e8efe9269cac924
Kanhaiya Bagul <bagulkanhaiya60@gmail.com>
2025-09-23 23:28:59 +0530

    Time: 2 ms (79.95%) | Memory: 13.8 MB (75.07%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
@@ -1,35 +1,27 @@
 class Solution {
 public:
-
-    void pathHelper(int src , int dest, vector<vector<int>>& graph,vector<vector<int>>& result, vector<int>& temp){
-        temp.push_back(src);
-        if(src == dest){
+    void pathHelper(int src, int dest, vector<vector<int>>& graph,
+                    vector<vector<int>>& result, vector<int>& temp) {
+        temp.push_back(src);//first push to the temp
+        if (src == dest) {// if src = dest then push to the result 
             result.push_back(temp);
-            temp.pop_back();
+            temp.pop_back();//for backtracking we have to pop it
             return;
         }
+        //loop for every node connected to the src
+        for (int v : graph[src]) {
 
-        
-        
-
-        for(int v : graph[src]){
-            
-                pathHelper(v, dest,graph, result, temp);
-            
+            pathHelper(v, dest, graph, result, temp);
         }
 
-        temp.pop_back();
-       
+        temp.pop_back();//back track 
     }
 
-    
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        int v = graph.size();
-        vector<vector<int>> result;
-        vector<int> temp;
-        pathHelper(0,v - 1, graph,result, temp);
+        int v = graph.size();//Total number of nodes
+        vector<vector<int>> result;//store the final result
+        vector<int> temp;//store temp path for every call
+        pathHelper(0, v - 1, graph, result, temp);//Helper fun call
         return result;
-
-        
     }
 };
