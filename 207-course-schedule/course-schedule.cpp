bc198755b215317f820d5ca3bef7dc03c06dc674
Kanhaiya Bagul <bagulkanhaiya60@gmail.com>
2025-09-24 20:00:49 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (86.16%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -6,41 +6,45 @@
     class Solution {
     public:
 
+        //helper fun 
         bool helper(int src, vector<bool>& vis, vector<bool>& recPath,vector<vector<int>>& graph){
-            vis[src] = true;
-            recPath[src] = true;
+            vis[src] = true;//mark as visited
+            recPath[src] = true;//mark for the cycle
 
+            //go through ever neighbout node
             for(int v : graph[src]){
-                if(!vis[v]){
-                    if(helper(v, vis, recPath, graph)){
+                if(!vis[v]){//if not visited
+                    if(helper(v, vis, recPath, graph)){//if it found the cycle return true
                         return true;
                     }
                 }else{
-                    if(recPath[v]){
+                    //if already visited and present in the recPath that means it is repeated and form the cycle 
+                    if(recPath[v]){//then return true
                         return true;
                     }
                 }
             }
-            recPath[src] = false;
-            return false;
+            recPath[src] = false;//this is for backtrack 
+            return false;//if no cycle is found then return false
         }
         bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
             int v = numCourses;
 
-            vector<bool> vis(v,false);
-            vector<bool> resPath(v,false);
+            vector<bool> vis(v,false);//tract for vis nodes
+            vector<bool> resPath(v,false);//tract the current path
 
 
-            vector<vector<int>> graph(numCourses);
+            vector<vector<int>> graph(numCourses);//this store the proper way of nodes and the neighbours
             for(auto &p : prerequisites){
                 int u = p[1];
                 int v = p[0];
                 graph[u].push_back(v);
 
             }
-
+            //this loop assure that every node is visited
             for(int i = 0; i < v; i++){
                 if(!vis[i]){
+                    //call for the helper and check if any cycle occure
                     if(helper(i, vis, resPath , graph)){
                         return false;
                     }
