1ca3e6460f3a54891e00601d9f8430c57102da58
Kanhaiya Bagul <bagulkanhaiya60@gmail.com>
2025-10-06 17:38:07 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (45.10%) - LeetSync

--- 1642-water-bottles/water-bottles.cpp
+++ 1642-water-bottles/water-bottles.cpp
@@ -1,10 +1,18 @@
 class Solution {
 public:
+    //randomly solve zala
     int numWaterBottles(int numBottles, int numExchange) {
+        //the number of bottel we drink at the first
         int result = numBottles;
+
+        //after drinking it become empty
         int emptyBottles = numBottles;
+        
+        //we run the loop untill no of empty bottle are less than the numExchange , we can't exchange no other bottle form it and stop the loop
         while (emptyBottles / numExchange != 0) {
+            //we add the number of bottle which can we drink
             result += emptyBottles / numExchange;
+            //after drinking the bottle it become empty add to the empty bottle , and if any remainig bottle are their also add them to the emptybottle
             emptyBottles = emptyBottles / numExchange + emptyBottles % numExchange;
         }
         return result;
