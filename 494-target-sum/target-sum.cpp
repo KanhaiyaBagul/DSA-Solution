4b47ed78ffd946c2e4c318a5e6738383813fecf6
Kanhaiya Bagul <bagulkanhaiya60@gmail.com>
2025-10-13 16:52:42 +0530

    Time: 395 ms (25.79%) | Memory: 89.8 MB (5.07%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     
-    unordered_map<string,int> dp;
+    unordered_map<string,int> dp;//this is use to backtrack it
     int helper(int i, vector<int>&A,int target, int sum){
         if(i == A.size()){
             return sum == target ? 1 : 0;
         }
 
         string key = to_string(i) + "," + to_string(sum);
-        if(dp.find(key) != dp.end()) return dp[key];
+        if(dp.find(key) != dp.end()) return dp[key]; // if already present then return it
         
         
-        int add = helper(i+1,A,target,sum + A[i]);
+        int add = helper(i+1,A,target,sum + A[i]);//add
         
-        int sub = helper(i+1,A,target,sum - A[i]);
+        int sub = helper(i+1,A,target,sum - A[i]);//sub
 
-        return dp[key] = add + sub;
+        return dp[key] = add + sub;//return and store in the dp
         
     }
     int findTargetSumWays(vector<int>& nums, int target) {
