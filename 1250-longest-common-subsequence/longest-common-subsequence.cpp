4c0d81cc2319ad3707bfd3af3753a1062db768fd
Kanhaiya Bagul <bagulkanhaiya60@gmail.com>
2025-10-17 15:21:17 +0530

    Time: 44 ms (16.89%) | Memory: 27.7 MB (15.39%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -6,23 +6,25 @@ public:
         if(i < 0 || j < 0){
             return 0;
         }
-
+        //if we already calculate it 
         if(dp[i][j] != -1){
             return dp[i][j];
         }
-
+        //if the last index match 
         if(text1[i] == text2[j]){
             return dp[i][j] = 1 + LCSHelper(i - 1, j -1 , text1, text2, dp);
-        }else{
+        }else{//if they not match then shrink both and pass the max ans
             int ans1 = LCSHelper(i - 1, j, text1, text2, dp);
             int ans2 =  LCSHelper(i, j -1 , text1, text2, dp);
             return dp[i][j] = max(ans1, ans2);
         }
     } 
     int longestCommonSubsequence(string text1, string text2) {
+        //this are the last index of the both the string
         int i = text1.size() - 1;
         int j = text2.size() - 1;
 
+        //this is the dp for memoization
         vector<vector<int>> dp(i + 1, vector<int> (j + 1, -1));
         return LCSHelper(i,j,text1,text2,dp);
         
