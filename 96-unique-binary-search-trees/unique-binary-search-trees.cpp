53ad6184a6f9e931b4c2be0af8b5ad7f5ef169f2
Kanhaiya Bagul <bagulkanhaiya60@gmail.com>
2025-10-20 20:54:22 +0530

    Time: 2 ms (18.93%) | Memory: 8.1 MB (38.68%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -1,5 +1,13 @@
 class Solution {
 public:
+    //for the catlan number 
+        //c0 = 1;
+        //c1 = 1;
+        //c2 = c0*c1 + c1*c0
+        //c3 = c2*c0 + c1*c1 + c0*c2
+        //cn = 0 -----> n-1
+        //          *
+        //      n-1 -----> 0
     int recursion(int n, vector<int>&dp){
         if(n == 0 || n  == 1){
             return 1;
@@ -8,6 +16,8 @@ public:
             return dp[n];
         }
         int ans = 0;
+        //it is the problme of catalans 
+        //we have to calculate like 
         for(int i = 0; i < n; i++){
             ans += recursion(i,dp) * recursion(n-i-1,dp);
         }
