8c295939023a86b0efcb2a03cf7df61f7bfdceee
Kanhaiya Bagul <bagulkanhaiya60@gmail.com>
2025-10-25 15:23:13 +0530

    Time: 15 ms (71.23%) | Memory: 22.8 MB (99.28%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-
-        
         unordered_map<string, int> s;
 
+        //this function store the unique string with the index
         int idx = 0;
         for(string str : strs){
             sort(str.begin(),str.end());
@@ -15,16 +14,17 @@ public:
             }
         }
 
+        //we find the identical element and store to the result with the help of the hash map 
         int rows = s.size();
         vector<vector<string>> result(rows);
 
         for(string str : strs){
-            string st = str;
-            sort(str.begin(),str.end());
-            string temp = str;
+            string oldString = str;//store the old string
+            sort(str.begin(),str.end());//sort the permanent
+            string temp = str;//this store the sorted string
 
-            int idx = s[temp];
-            result[idx].push_back(st);
+            int idx = s[temp];//this is the index of the result 2d vector
+            result[idx].push_back(oldString);//we push it accordingly
         }
         return result;
     }
