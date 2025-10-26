dd7b59c95f00b668fe4f50415d987d866c77fd0f
Kanhaiya Bagul <bagulkanhaiya60@gmail.com>
2025-10-26 07:04:11 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (98.08%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -2,15 +2,16 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
-        int seq = 0;
-
+        int idx = 0;
+        
         for(int i = 0; i < n; i++){
             if(nums[i] != 0){
-                swap(nums[i] , nums[seq]);
-                seq++;
-
+                nums[idx++] = nums[i];
             }
         }
         
+        for(int i = idx; i < n; i++){
+            nums[i] = 0;
+        }   
     }
 };
