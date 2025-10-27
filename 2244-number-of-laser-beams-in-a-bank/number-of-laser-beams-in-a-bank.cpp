31ed00349f429e8f979c99c0682d6f2a84dac8ea
Kanhaiya Bagul <bagulkanhaiya60@gmail.com>
2025-10-27 07:18:05 +0530

    Time: 17 ms (10.16%) | Memory: 28.1 MB (35.40%) - LeetSync

--- 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.cpp
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.cpp
@@ -6,9 +6,9 @@ public:
         int m = bank[0].size();
         int laser = 0;
 
-        vector<int> store(n,0);
+        vector<int> store(n,0);//store the data, in which row how many devices are their
 
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < n; i++){//this use to go through all the places
             int security = 0;
             for(int j = 0; j < m; j++){
                 
@@ -18,14 +18,15 @@ public:
                 store[i] = security;
             }
         }
-        vector<int> arr;
+
+        vector<int> arr;//this arr store the non zero rows which has the devices
         for(int i = 0; i < store.size(); i++){
             if(store[i] != 0){
                 arr.push_back(store[i]);
             }
         }
 
-        int result = 0;
+        int result = 0;//this simplly calculat the laser beem using the simple formula
         for(int i = 1; i < arr.size(); i++){
             result += arr[i-1] * arr[i];
         }
