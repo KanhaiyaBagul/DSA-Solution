4083d832ad97768ae7fdf9e535a97fb091508cb8
Kanhaiya Bagul <bagulkanhaiya60@gmail.com>
2025-10-28 16:30:08 +0530

    Time: 43 ms (85.93%) | Memory: 29.1 MB (48.66%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,37 +1,42 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-
         sort(nums.begin(),nums.end());
+        vector<vector<int>> result;
 
-        vector<vector<int>> ans;
         int n = nums.size();
-        
-        for (int i = 0; i < n ; i++){
-            if(i > 0 && nums[i] == nums[i-1]) continue;
 
+        for(int i = 0; i < n; i++){
+            if(i > 0 && nums[i] == nums[i-1]){
+                continue;//skip the duplicate element
+            }
+
+            //this pointer is use to solve smaller problem
             int j = i + 1;
-            int k = n - 1 ;
-             while (j<k){
+            int k = n - 1;
+
+            while(j < k){
                 int sum = nums[i] + nums[j] + nums[k];
 
-                if(sum > 0){
-                    k --;
-                }
-                else if (sum < 0){
-                    j ++;
-                }
-
-                else{
-                    ans.push_back({nums[i] , nums[j] , nums[k]});
+                if(sum > 0){//if sum > 0 means we have to reduce it 
+                    k--;
+                }else if(sum < 0){//increment the sum
+                    j++;
+                }else{
+                    result.push_back({nums[i],nums[j], nums[k]});//if sum == 0
+                    //shrink window size
                     j++;
                     k--;
 
-                    while(j<k && nums[j] == nums[j-1]) j++;
+                    while(j<k && nums[j] == nums[j-1]){//remove the duplicate
+                        j++;
+                    }
                 }
-             }
+            }
+            
         }
-        return ans;
+        return result;
+        
         
     }
 };
