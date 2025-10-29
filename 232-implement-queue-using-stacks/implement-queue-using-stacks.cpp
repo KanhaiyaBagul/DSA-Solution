e7bc5b3e12065cb147ed8c08db5c779f72f600ee
Kanhaiya Bagul <bagulkanhaiya60@gmail.com>
2025-10-29 23:13:01 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (66.49%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -1,16 +1,16 @@
 class MyQueue {
     stack<int> s1;
     stack<int> s2;
+
+
 public:
     MyQueue() {
         while(!s1.empty()) s1.pop();
         while(!s2.empty()) s2.pop();
-
         
     }
     
     void push(int x) {
-        //s1 -> s2
         while(!s1.empty()){
             s2.push(s1.top());
             s1.pop();
@@ -18,32 +18,34 @@ public:
 
         s1.push(x);
 
-        //s2 -> s1
         while(!s2.empty()){
             s1.push(s2.top());
             s2.pop();
         }
 
         
-        
     }
     
     int pop() {
-        if(s1.empty()) return -1;
-        int frontElement = s1.top();
-        s1.pop();
-        return frontElement;
-        
+        if(!s1.empty()){
+            int num = s1.top();
+            s1.pop();
+            return num;
+
+        }
+        return -1;
     }
     
     int peek() {
-        if(s1.empty()) return -1;
-        return s1.top();
+        if(!s1.empty()){
+            return s1.top();
+        }
+        return -1;
         
     }
     
     bool empty() {
-        return s1.empty();
+        return s1.size() == 0;
         
     }
 };
