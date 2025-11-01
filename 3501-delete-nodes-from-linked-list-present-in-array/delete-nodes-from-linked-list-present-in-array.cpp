0e507afd2eec4bd45c61773e5c38592226a3ef58
Kanhaiya Bagul <bagulkanhaiya60@gmail.com>
2025-11-01 16:19:29 +0530

    Time: 125 ms (28.69%) | Memory: 279.5 MB (7.41%) - LeetSync

--- 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.cpp
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.cpp
@@ -12,24 +12,24 @@ class Solution {
 public:
     ListNode* modifiedList(vector<int>& nums, ListNode* head) {
         int n = nums.size();
-        //first use the map to store the element
+        //store the nums in the set
         unordered_set<int> s(nums.begin(),nums.end());
         
 
-        ListNode dummy;
-        ListNode* temp = &dummy;
+        ListNode dummy;//create a dummy node
+        ListNode* temp = &dummy;//create new ListNode and it is pointing to the address of the dummy node
 
         while(head != nullptr){
 
-            if(s.find(head -> val) == s.end()){
+            if(s.find(head -> val) == s.end()){//if the node is not present in the set the add to the new list node
                 ListNode* l = new ListNode(head -> val);
-                temp -> next = l;
-                temp = temp -> next;
+                temp -> next = l;//point the new node to the list
+                temp = temp -> next;//move forward the temp
             }
-            head = head -> next;
+            head = head -> next;//also move the head pointer  , which is use for traversel
         }
 
-        return dummy.next;
+        return dummy.next;//this return the starting node of our resultent 
 
         
 
