ebb440f597492f7afbeeb6d92f3036ba89916573
Kanhaiya Bagul <bagulkanhaiya60@gmail.com>
2025-11-02 14:06:40 +0530

    Time: 7 ms (28.21%) | Memory: 12 MB (28.75%) - LeetSync

--- 383-ransom-note/ransom-note.cpp
+++ 383-ransom-note/ransom-note.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        vector<int> freqMagazine(26,0);
+
+        unordered_map<char, int> freqMagazine;
 
         for(char ch : magazine){
-            freqMagazine[ch - 'a']++;
-
+            freqMagazine[ch]++;
         }
 
         for(char ch : ransomNote){
-            if(freqMagazine[ch - 'a'] == 0){
-                return false ;
+            if(freqMagazine[ch] == 0){
+                return false;
             }
 
-            freqMagazine[ch - 'a']--;
+            freqMagazine[ch]--;
         }
-
         return true;
+
+
         
     }
 };
