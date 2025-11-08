82185e464205eecb514cacefe1d35031f27ec771
Kanhaiya Bagul <bagulkanhaiya60@gmail.com>
2025-11-08 10:32:43 +0530

    Time: 3 ms (69.28%) | Memory: 9.9 MB (86.35%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
+
         if(s1.size() > s2.size()) return false;
-
-        vector<int> s1count(26,0);
-        vector<int> s2count(26,0);
+        int n = s2.size();
         int left = 0;
+        vector<int> freq1(26,0);
+        vector<int> freq2(26,0);
 
-        for(int i = 0; i < s1.size(); i++){
-            s1count[s1[i] - 'a'] ++;
-            s2count[s2[i] - 'a'] ++;
-
+        for(int i = 0; i < s1.size();i++){
+            freq1[s1[i] - 'a']++;
+            freq2[s2[i] - 'a']++;
         }
 
-        if(s1count == s2count) return true;
-
-        for(int i = s1.size(); i < s2.size(); i ++){
-
-            s2count[s2[left] - 'a']--;
-            s2count[s2[i] - 'a']++;
-            if(s1count == s2count) return true;
-            left++;
-
-        }
+        if(freq1 == freq2) return true;
         
 
+        for(int right = s1.size(); right < s2.size(); right++){
+
+            freq2[s2[left] - 'a']--;
+            freq2[s2[right] - 'a']++;
+
+            if(freq2 == freq1){
+                return true;
+            }
+            left ++;
+            
+        }    
         return false;
-        
+
     }
 };
