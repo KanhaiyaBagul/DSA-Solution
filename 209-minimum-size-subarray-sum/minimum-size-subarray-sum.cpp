fd88982f25672e6ee7abdb975dee94f091662ba0
Kanhaiya Bagul <bagulkanhaiya60@gmail.com>
2025-11-09 23:17:38 +0530

    Time: 0 ms (100.00%) | Memory: 32.1 MB (39.20%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
+        //in this problem we have to find min len of array which sum is greater than target
         int n = nums.size();
         int left = 0;
-
+        //we find the right condition, like first find the sum which is greater or equal to the target here, then try to shrink the solution window if it strinke then calculate the new minLen
         int sum = 0;
         int minLen = INT_MAX;
         for(int right = 0; right < n; right++){
