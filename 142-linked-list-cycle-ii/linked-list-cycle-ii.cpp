33988a8511c568733582c1ab0e9638b52936b37d
Kanhaiya Bagul <bagulkanhaiya60@gmail.com>
2025-11-14 14:31:44 +0530

    Time: 10 ms (46.11%) | Memory: 11.4 MB (51.96%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -9,35 +9,23 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-
         ListNode* slow = head;
-        ListNode * fast = head;
-        bool isCycle = false;
-        //We check if the cycle is formed or not
+        ListNode* fast = head;
+
         while(fast != nullptr && fast -> next != nullptr){
             slow = slow -> next;
             fast = fast -> next -> next;
 
             if(slow == fast){
-            isCycle = true;
-            break;
+                slow = head;
+                while(slow != fast){
+                    slow = slow -> next;
+                    fast = fast -> next;
+                }
+                return slow;
             }
         }
-
-        if(!isCycle) return nullptr;//If no cycle is formed
-
-        slow = head;//if the cycle is formed them initialize the slow to the head
-
-        //then move the fast and slow by 1
-        //There is the conditon we met both the slow and the fast at the starting point of the loop 
-        while(slow != fast){
-            slow = slow -> next;
-            fast = fast -> next;
-        }
-
-        return slow;//return the the slow 
-
-        
+        return nullptr;
         
     }
 };
