b35cf288150874771e476a95445862529b013868
Kanhaiya Bagul <bagulkanhaiya60@gmail.com>
2025-11-14 19:13:39 +0530

    Time: 11 ms (14.47%) | Memory: 124.9 MB (35.76%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -10,28 +10,34 @@
  */
 class Solution {
 public:
-
     bool isPalindrome(ListNode* head) {
-      stack<int> st;
+        stack<int> s;
 
-      ListNode *temp = head;
-        //store element in the stack
-      while(temp != nullptr){
-        st.push(temp->val);
-        temp = temp->next;
-      }
-
-        //check if the the stack element and the stack element are equal 
-      ListNode * curr = head;
-      while(curr != nullptr){
-        if(curr->val != st.top()){
-            return false;//if not equal if not then return the false
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast != nullptr && fast -> next != nullptr){
+            s.push(slow -> val);
+            slow = slow -> next;
+            fast = fast -> next -> next;
         }
-        st.pop();//remove element from stack if the the values in the ll and the stack are equla 
-        curr = curr->next;
 
-      }
-      return true;  
+        if(fast != nullptr){//if fast is not null means list is even then skip the middle element
+            slow = slow -> next;
+        }
+    
+        while(!s.empty()){
+            if(s.top() == slow -> val){
+                s.pop();
+                slow = slow -> next;
+            }
+            else{
+                return false;
+            }
+
+        }
+
+        return true;
+
         
     }
 };
