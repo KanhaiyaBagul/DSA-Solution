510de29258ba4eaaa65bdd560b509f202c6dfa24
Kanhaiya Bagul <bagulkanhaiya60@gmail.com>
2025-11-15 13:20:33 +0530

    Time: 0 ms (100.00%) | Memory: 22.8 MB (56.83%) - LeetSync

--- 143-reorder-list/reorder-list.cpp
+++ 143-reorder-list/reorder-list.cpp
@@ -10,77 +10,69 @@
  */
 class Solution {
 public:
-
-    //reverse the right sub ll
-    ListNode * reverse(ListNode*right){
-        ListNode * curr = right;
-        ListNode * prev = nullptr;
-        ListNode * next = nullptr;
+    //reverse fun
+    ListNode* reverse(ListNode* head){
+        ListNode* curr = head;
+        ListNode* prev = nullptr;
+        ListNode* next = nullptr;
 
         while(curr != nullptr){
             next = curr -> next;
             curr -> next = prev;
-
+            
             prev = curr;
             curr = next;
+
         }
         return prev;
     }
-
-    //divide the ll
-    ListNode *getMid(ListNode* head){
-        ListNode * slow = head;
-        ListNode * fast = head;
-        ListNode * prev = nullptr;
+    //calculat the mid
+    ListNode* getMid(ListNode* head){
+        ListNode* slow = head;
+        ListNode* fast = head;
+        ListNode* prev = nullptr;
 
         while(fast != nullptr && fast -> next != nullptr){
-            prev = slow;
+            prev = slow;//it store the previous of slow , so we can break the first half 
             slow = slow -> next;
             fast = fast -> next -> next;
-
         }
-         // Break the list into two halves only if prev is not nullptr
-        if (prev != nullptr) {
-            prev->next = nullptr;
+        if(prev != nullptr){
+            prev -> next = nullptr;//so it break the list into half
         }
-
         return slow;
     }
     void reorderList(ListNode* head) {
+        //in this problem we divive it into 3 steps
+        //step 1 : divide the whole LL into half
+        //step 2 : reverse the right half
+        //step 3 : merget the two LL
 
-        ListNode * leftHead = head;
-        ListNode * rightHead = getMid(head);
+        ListNode* left = head;//this is the head of the left LL
+        ListNode* right = getMid(head);//find the mid of other LL
 
-        ListNode * rightreverse = reverse(rightHead);
-        ListNode * tail = rightreverse;
+        ListNode* reverseRight = reverse(right);//Reverse it
 
+        ListNode* tail = reverseRight;//this is use when we have diff size of the LL
+        while(left != nullptr && reverseRight != nullptr){
+            //first we store the next element of the each LL 
+            ListNode* leftNext = left -> next;
+            ListNode* rightNext = reverseRight -> next;
 
-        //This is the core logic of merging the two ll
-        while(leftHead != nullptr && rightreverse != nullptr){
-            //we created two pointers which points the next of the current left and the right it because if update the node position the connection between the next node is disconected and we don't have the access of them 
-            ListNode * nextLeft = leftHead -> next;
-            ListNode* nextRight = rightreverse -> next;
+            //point left next to the reverse right
+            left -> next = reverseRight;
+            //point the reverse to the next element of the left LL
+            reverseRight -> next = leftNext;
 
-
-            //left node point the right 
-            leftHead -> next = rightreverse;
-            //right points to the next element of the left
-            rightreverse -> next = nextLeft;
-
-            tail = rightreverse;
-
-            //we update our nodes to continue our process
-            leftHead = nextLeft;
-            rightreverse = nextRight;
-
+            tail = reverseRight;// the tail is alway the reverseRight
+            //update the value of the nodes to the next of each
+            left = leftNext;
+            reverseRight = rightNext;
         }
-
-        if(rightreverse != nullptr){
-            tail -> next = rightreverse;
+        if(tail != nullptr){//if the tail is not null means some elemente are still remaining
+            //so attach the remainig to the tail
+            tail -> next = reverseRight;
         }
-
-        return;
-
-        
+        return;    
     }
 };
