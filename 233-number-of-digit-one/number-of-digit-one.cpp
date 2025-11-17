32443aeb5643b09ff54745673115871abadb6c0a
Kanhaiya Bagul <bagulkanhaiya60@gmail.com>
2025-11-17 19:56:39 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (51.45%) - LeetSync

--- 233-number-of-digit-one/number-of-digit-one.cpp
+++ 233-number-of-digit-one/number-of-digit-one.cpp
@@ -7,13 +7,13 @@ public:
             return cnt;
         }
 
-        if(dp[idx][tight][cnt] != -1) return dp[idx][tight][cnt];
+        if(dp[idx][tight][cnt] != -1) return dp[idx][tight][cnt]; //memoization
 
-        int limit = (tight == 1 ? s[idx] - '0' : 9);
+        int limit = (tight == 1 ? s[idx] - '0' : 9);//this is for the restriction
         int ans = 0;
 
         for(int i = 0; i <= limit; i++){
-            int updatedCnt = cnt + (i == 1 ? 1 : 0);
+            int updatedCnt = cnt + (i == 1 ? 1 : 0);//calculate the updated count
             ans += solve(s,idx + 1, (tight && (i == s[idx] - '0')),updatedCnt);
         }
         return dp[idx][tight][cnt] = ans;
@@ -21,8 +21,7 @@ public:
     }
     int countDigitOne(int n) {
         string s = to_string(n);
-        memset(dp,-1,sizeof(dp));
-
+        memset(dp,-1,sizeof(dp));//intilize the array
         return solve(s,0,1,0);
         
     }
