44c4111ea83273999091cf718598b6f15b6c2423
Kanhaiya Bagul <bagulkanhaiya60@gmail.com>
2025-11-18 07:04:02 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (25.84%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-
+    //using kadang algo 
+    //calculate the minsum of the whole array
     int minSum(vector<int> & nums){
         
         int bestEnding = nums[0];
@@ -12,6 +13,7 @@ public:
         }
         return result;
     }
+    //calculate maxSum of array
     int maxSum(vector<int> & nums){
         int bestEnding = nums[0];
         int result = nums[0];
@@ -23,6 +25,8 @@ public:
         return result;
     }
     int maxAbsoluteSum(vector<int>& nums) {
+        //here we calculate the max Sum and the min sum of the array 
+        //and compair which is the max 
         return max(maxSum(nums), abs(minSum(nums)));
         
     }
