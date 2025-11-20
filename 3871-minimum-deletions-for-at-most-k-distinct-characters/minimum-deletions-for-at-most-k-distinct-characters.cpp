4527f4b9cfd1b4fdd18902bf5ebe342eb4880345
Kanhaiya Bagul <bagulkanhaiya60@gmail.com>
2025-11-20 16:04:55 +0530

    Time: 2 ms (46.26%) | Memory: 9.8 MB (57.94%) - LeetSync

--- 3871-minimum-deletions-for-at-most-k-distinct-characters/minimum-deletions-for-at-most-k-distinct-characters.cpp
+++ 3871-minimum-deletions-for-at-most-k-distinct-characters/minimum-deletions-for-at-most-k-distinct-characters.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-    //solved by min heap
     int minDeletion(string s, int k) {
-        unordered_map<char, int> freq;
-        for (char ch : s) {
+        unordered_map<char , int> freq;
+        for(char ch : s){
             freq[ch]++;
         }
 
-        priority_queue<int, vector<int>, greater<int>> pq;
-        for (auto& pair : freq) {
-            pq.push(pair.second);
+        vector<int> arr; //we here store the freq and then sort it
+        for(auto& pair : freq){
+            arr.push_back(pair.second);
         }
 
+        sort(arr.begin(),arr.end());
+
+        int distinct = arr.size();
         int result = 0;
-        if (pq.size() > k) {
-            while (pq.size() != k) {
-                result += pq.top();
-                pq.pop();
-            }
+        int idx = 0;
+        while(distinct > k){
+            result += arr[idx++];
+            distinct --; 
         }
         return result;
+        
     }
 };
