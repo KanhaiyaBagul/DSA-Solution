0b4ef140784491337ac1a6ee6d5e7b50bc19089d
Kanhaiya Bagul <bagulkanhaiya60@gmail.com>
2025-11-20 16:01:58 +0530

    Time: 4 ms (19.39%) | Memory: 9.9 MB (42.06%) - LeetSync

--- 3871-minimum-deletions-for-at-most-k-distinct-characters/minimum-deletions-for-at-most-k-distinct-characters.cpp
+++ 3871-minimum-deletions-for-at-most-k-distinct-characters/minimum-deletions-for-at-most-k-distinct-characters.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
+    //solved by min heap
     int minDeletion(string s, int k) {
         unordered_map<char, int> freq;
-        for(char ch : s){
+        for (char ch : s) {
             freq[ch]++;
         }
-        
-        priority_queue<int , vector<int> , greater<int>>pq;
-        for(auto& pair : freq){
+
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for (auto& pair : freq) {
             pq.push(pair.second);
         }
 
         int result = 0;
-        if(pq.size() > k){
-            while(pq.size() != k){
-            result+=pq.top();
-            pq.pop();
-            
-        }
+        if (pq.size() > k) {
+            while (pq.size() != k) {
+                result += pq.top();
+                pq.pop();
+            }
         }
         return result;
     }
