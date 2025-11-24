a943adf3bc707738683792ae2a42952f1dcfa626
Kanhaiya Bagul <bagulkanhaiya60@gmail.com>
2025-11-24 12:17:06 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (69.40%) - LeetSync

--- 1071-binary-prefix-divisible-by-5/binary-prefix-divisible-by-5.cpp
+++ 1071-binary-prefix-divisible-by-5/binary-prefix-divisible-by-5.cpp
@@ -5,7 +5,7 @@ public:
         vector<bool> result(nums.size());
 
         for(int i = 0; i < nums.size();i++){
-            num = (num * 2) % 5 + nums[i];
+            num = (num * 2) % 5 + nums[i];//this left the value  it is use full to add the nums element as bit
 
             if(num % 5 == 0){
                 result[i] = true;
