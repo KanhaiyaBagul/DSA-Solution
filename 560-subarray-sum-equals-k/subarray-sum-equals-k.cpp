85b53c42c1acd952e4c34c359b72c34115dbe269
Kanhaiya Bagul <bagulkanhaiya60@gmail.com>
2025-11-25 09:17:09 +0530

    Time: 33 ms (92.41%) | Memory: 45.3 MB (69.27%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
+    //this is solved using the prefix sum
     int subarraySum(vector<int>& nums, int k) {
         int sum = 0;
-        unordered_map<int,int> f;
+        unordered_map<int,int> f;//it stores the prefix sum for each index i
         int result = 0;
 
-        f[0] = 1;
+        f[0] = 1;//this is for the empty sub array whose sum is "0"
         for(int i = 0;i < nums.size();i++){
             sum += nums[i];
-            int freq = f.find(sum - k) != f.end() ? f[sum - k] : 0;
-            result += freq;
+            int freq = f.find(sum - k) != f.end() ? f[sum - k] : 0;//if we find the sum - k means theris is a sub array whose equal to the k
+            result += freq;//add the freq here 
             f[sum]++;
         }
         return result;
