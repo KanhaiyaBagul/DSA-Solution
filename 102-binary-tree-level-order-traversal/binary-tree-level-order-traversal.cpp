561fe2511511e09d88f0ba5103056e94c9d56c73
Kanhaiya Bagul <bagulkanhaiya60@gmail.com>
2025-11-25 16:48:05 +0530

    Time: 0 ms (100.00%) | Memory: 17.1 MB (70.61%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -6,48 +6,36 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>> result;
-        //if root is null
         if(root == nullptr){
             return {};
         }
-        //create queue for level traversal
-        queue<TreeNode*> Q;
-        //push root to the queue
-        Q.push(root);
 
+        queue<TreeNode*> q;
+        vector<vector<int>> result;
+        q.push(root);
 
-        while(!Q.empty()){
-            //size of the queue
-            int levelSize = Q.size();
-            //this stores the level wise int
+        while (!q.empty()) {
+            int size = q.size();
             vector<int> temp;
-
-            //traverse for the only element who are in the levell
-            for(int i = 0; i < levelSize; i++){
-                TreeNode* curr = Q.front();
-                Q.pop();
-                //push every element to the temp 
-                temp.push_back(curr -> val);
-
-                if(curr -> left != nullptr){
-                    Q.push(curr -> left);
+            for (int i = 0; i < size; i++) {
+                temp.push_back(q.front() -> val);
+                if (q.front() -> left) {
+                    q.push(q.front()->left);
                 }
-                if(curr -> right != nullptr){
-                    Q.push(curr -> right);
+                if (q.front() -> right) {
+                    q.push(q.front()->right);
                 }
+                q.pop();
             }
-
             result.push_back(temp);
         }
         return result;
-
-        
     }
 };
