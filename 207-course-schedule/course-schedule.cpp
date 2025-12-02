3b52de5b955af072d550bcce3bce5ae841102674
Kanhaiya Bagul <bagulkanhaiya60@gmail.com>
2025-12-02 11:24:23 +0530

    Time: 3 ms (78.75%) | Memory: 19.2 MB (83.88%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,56 +1,48 @@
-    //in this question we have to return that if we can take the course 
-    //We can attend ,if we do the 2nd course before 1 
-    //we have to apply topological sort here , for that we require the Directed Acyclic graph 
-    //if the cycle occure then it fail to attend the courses 
-    //so here we just have to find it is directed and acyclic 
-    class Solution {
-    public:
+class Solution {
+public:
+    bool dfs(int u,vector<vector<int>>& grid,vector<bool>& vis,vector<bool>& path){
+        
+        vis[u] = true;
 
-        //helper fun 
-        bool helper(int src, vector<bool>& vis, vector<bool>& recPath,vector<vector<int>>& graph){
-            vis[src] = true;//mark as visited
-            recPath[src] = true;//mark for the cycle
+        path[u] = true;
 
-            //go through ever neighbout node
-            for(int v : graph[src]){
-                if(!vis[v]){//if not visited
-                    if(helper(v, vis, recPath, graph)){//if it found the cycle return true
-                        return true;
-                    }
-                }else{
-                    //if already visited and present in the recPath that means it is repeated and form the cycle 
-                    if(recPath[v]){//then return true
-                        return true;
-                    }
+        for(auto v : grid[u]){
+            if(!vis[v]){
+                if(dfs(v,grid,vis,path)){
+                    return true;
+                }
+            }else if(path[v]){
+                return true;
+            }
+        }
+
+        path[u] = false;
+        return false;
+
+
+
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        int v = numCourses;
+        vector<bool> vis(v,false);
+        vector<bool> path(v,false);
+
+        vector<vector<int>> grid(v);
+
+        for(auto& pair : prerequisites){
+            int u = pair[0];
+            int v = pair[1];
+
+            grid[u].push_back(v);
+        }
+
+        for(int i = 0; i < v; i++){
+            if(!vis[i]){
+                if(dfs(i,grid,vis,path)){
+                    return false;
                 }
             }
-            recPath[src] = false;//this is for backtrack 
-            return false;//if no cycle is found then return false
         }
-        bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-            int v = numCourses;
-
-            vector<bool> vis(v,false);//tract for vis nodes
-            vector<bool> resPath(v,false);//tract the current path
-
-
-            vector<vector<int>> graph(numCourses);//this store the proper way of nodes and the neighbours
-            for(auto &p : prerequisites){
-                int u = p[1];
-                int v = p[0];
-                graph[u].push_back(v);
-
-            }
-            //this loop assure that every node is visited
-            for(int i = 0; i < v; i++){
-                if(!vis[i]){
-                    //call for the helper and check if any cycle occure
-                    if(helper(i, vis, resPath , graph)){
-                        return false;
-                    }
-                }
-            }
-            return true;
-            
-        }
-    };
+        return true;
+    }
+};
