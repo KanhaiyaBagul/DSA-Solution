fec157a9230c3f41bc7a4a5aaf07075c4e665aae
Kanhaiya Bagul <bagulkanhaiya60@gmail.com>
2025-12-03 07:18:19 +0530

    Time: 6 ms (97.78%) | Memory: 23.3 MB (96.38%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,55 +1,55 @@
 class Solution {
 public:
+    int m, n;
+
+    bool isSafe(int r, int c){
+        return r >=0 && c >= 0 && r < m && c < n;
+    }
+
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int sr = 0;
-        int sc = 0;
+        m = grid.size();
+        n = grid[0].size();
 
-        
+        if (m == 0 || n == 0 || grid[0][0] != 0) return -1;
 
-        int n = grid.size();
-        int m = grid[0].size();
+        queue<pair<int, int>> q;
+        q.push({0, 0});
 
-        if (n == 1 && m == 1) return grid[0][0] == 0 ? 1 : -1;
+        int x[] = {-1,-1,0,1,1,1,0,-1};
+        int y[] = {0,1,1,1,0,-1,-1,-1};
+        int level = 0;
 
-// If start or end is blocked
-if (grid[0][0] == 1 || grid[n-1][m-1] == 1) return -1;
+        while (!q.empty()) {
 
+            int size = q.size();
 
+            for (int i = 0; i < size; i++) {
+                auto curr = q.front();
+                q.pop();
 
-        
+                int r = curr.first;
+                int c = curr.second;
 
-        vector<vector<int>> dist(n, vector<int> (m , INT_MAX));
-        dist[sr][sc] = 1;
+                if(r == m - 1 && c == n - 1){
+                    return level + 1;
+                }
 
-        queue<pair<int, pair<int, int>>> q;
-        q.push({1,{sr,sc}});
+                for (int k = 0; k < 8; k++) {
 
-        vector<int> dr = {-1,-1,0,1,1,1,0,-1};
-        vector<int> dc = {0,1,1,1,0,-1,-1,-1};
+                    int nr = r + x[k];
+                    int nc = c + y[k];
 
-        while(!q.empty()){
-            auto it = q.front();
-            q.pop();
-            int dista = it.first;
-            int r = it.second.first;
-            int c = it.second.second;
+                    if(isSafe(nr,nc) && grid[nr][nc] == 0){
+                        q.push({nr,nc});
+                        grid[nr][nc] = 1;
 
-            for(int i = 0; i < 8; i++){
-                int newr = r + dr[i];
-                int newc = c + dc[i];
-
-                if(newr >= 0 && newr < n && newc >= 0 && newc < m && grid[newr][newc] == 0 && dista + 1 < dist[newr][newc]){
-                    dist[newr][newc] = dista + 1;
-                    if(newr ==  n - 1 && newc ==  n - 1){
-                        return dista + 1;
                     }
-                    q.push({dista + 1,{newr,newc}});
+
                 }
             }
 
-
+            level++;
         }
         return -1;
-        
     }
 };
