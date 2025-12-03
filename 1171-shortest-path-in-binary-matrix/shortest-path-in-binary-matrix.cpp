10f5b3705437d2ec30af43ca0654bc1df96817d9
Kanhaiya Bagul <bagulkanhaiya60@gmail.com>
2025-12-03 07:48:40 +0530

    Time: 38 ms (27.42%) | Memory: 23.4 MB (91.29%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
-    //we use BFS 
-    //increase level for each level;
-    int m, n;
+    int m,n;
+
     bool isSafe(int r, int c){
         return r >=0 && c >= 0 && r < m && c < n;
     }
@@ -11,47 +10,42 @@ public:
         m = grid.size();
         n = grid[0].size();
 
-        if (m == 0 || n == 0 || grid[0][0] != 0) return -1;
+        if(grid[0][0] == 1 || grid[m-1][n-1]) return -1;
 
-        queue<pair<int, int>> q;
-        q.push({0, 0});//initial condition
+        using P = pair<int, pair<int,int>>;
 
-        //moving in 8 direction
+        vector<vector<int>> dist(m,vector<int> (n,INT_MAX));
+        dist[0][0] = 0;
+
+        priority_queue<P,vector<P>, greater<P>> pq;
+
+        pq.push({0,{0,0}});
+
         int x[] = {-1,-1,0,1,1,1,0,-1};
         int y[] = {0,1,1,1,0,-1,-1,-1};
-        int level = 0;
 
-        while (!q.empty()) {
+        while(!pq.empty()){
+            auto curr = pq.top();
+            pq.pop();
 
-            int size = q.size();//initial size of the queue
+            int d = curr.first;
+            int r = curr.second.first;
+            int c = curr.second.second;
 
-            for (int i = 0; i < size; i++) {
-                auto curr = q.front();//get the firstt element
-                q.pop();//pop it
+            for(int i = 0; i < 8; i++){
+                int nr = r + x[i];
+                int nc = c + y[i];
 
-                int r = curr.first;
-                int c = curr.second;
-
-                if(r == m - 1 && c == n - 1){
-                    return level + 1;//if the r and c reach the last index return the level
-                }
-
-                for (int k = 0; k < 8; k++) {
-
-                    int nr = r + x[k];
-                    int nc = c + y[k];
-
-                    if(isSafe(nr,nc) && grid[nr][nc] == 0){
-                        q.push({nr,nc});
-                        grid[nr][nc] = 1;
-
+                if(isSafe(nr,nc) && grid[nr][nc] == 0){
+                    if(dist[nr][nc] > d + 1){
+                        dist[nr][nc] = d + 1;
+                        pq.push({d+1,{nr,nc}});
                     }
-
                 }
             }
 
-            level++;
         }
-        return -1;
+        return dist[m-1][n-1] == INT_MAX ? -1 : dist[m-1][n-1] + 1;
+        
     }
 };
