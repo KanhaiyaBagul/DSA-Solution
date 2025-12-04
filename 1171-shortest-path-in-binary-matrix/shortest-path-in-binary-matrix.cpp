6cbe2e77cb2b35983bf70ef8871d53dec41ea75b
Kanhaiya Bagul <bagulkanhaiya60@gmail.com>
2025-12-04 07:34:38 +0530

    Time: 27 ms (38.51%) | Memory: 23.6 MB (83.56%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,51 +1,56 @@
 class Solution {
 public:
-    int m,n;
-
+    int m, n;
     bool isSafe(int r, int c){
-        return r >=0 && c >= 0 && r < m && c < n;
+        return r >= 0 && c >= 0 && r < m && c < n;
     }
-
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         m = grid.size();
         n = grid[0].size();
 
-        if(grid[0][0] == 1 || grid[m-1][n-1]) return -1;
-
-        using P = pair<int, pair<int,int>>;
+        if(grid[0][0] == 1 || grid[m-1][n-1] == 1) return -1;
 
         vector<vector<int>> dist(m,vector<int> (n,INT_MAX));
+
         dist[0][0] = 0;
 
-        priority_queue<P,vector<P>, greater<P>> pq;
+        using P = pair<int,pair<int,int>>;
+
+        priority_queue<P,vector<P>,greater<P>> pq;
 
         pq.push({0,{0,0}});
 
-        int x[] = {-1,-1,0,1,1,1,0,-1};
-        int y[] = {0,1,1,1,0,-1,-1,-1};
+        int x[] = {-1, -1, -1,  0, 0,  1, 1, 1};
+        int y[] = {-1,  0,  1, -1, 1, -1, 0, 1};
 
         while(!pq.empty()){
             auto curr = pq.top();
             pq.pop();
 
-            int d = curr.first;
+            int len = curr.first;
             int r = curr.second.first;
             int c = curr.second.second;
+
+            
 
             for(int i = 0; i < 8; i++){
                 int nr = r + x[i];
                 int nc = c + y[i];
 
                 if(isSafe(nr,nc) && grid[nr][nc] == 0){
-                    if(dist[nr][nc] > d + 1){
-                        dist[nr][nc] = d + 1;
-                        pq.push({d+1,{nr,nc}});
+                    if(dist[nr][nc] > len + 1){
+                        grid[nr][nc] = 1;
+                        dist[nr][nc] = len + 1;
+                        pq.push({len + 1,{nr,nc}});
                     }
+                    
                 }
+
             }
+
 
         }
         return dist[m-1][n-1] == INT_MAX ? -1 : dist[m-1][n-1] + 1;
-        
+
     }
 };
