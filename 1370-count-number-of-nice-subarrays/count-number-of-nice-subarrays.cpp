fe72dd9c5e35efdcd4b303843f3866110b44c087
Kanhaiya Bagul <bagulkanhaiya60@gmail.com>
2025-12-05 13:35:57 +0530

    Time: 84 ms (12.58%) | Memory: 94.2 MB (26.48%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -9,12 +9,15 @@ public:
         int result = 0;
 
         for(int i = 0; i < nums.size();i++){
+            //if the num is even then add "0" if odd then "1"
             sum += (nums[i] % 2 == 0) ? 0 : 1;
 
+            //find the count of the sum - k freq in the hash table 
             int f = freq.count(sum - k) == 0 ? 0 : freq[sum - k];
 
-            result += f;
-            freq[sum]++;
+            result += f;//add the freq of the sum - k element
+
+            freq[sum]++;//add the founded sum here 
 
         }
         return result;
