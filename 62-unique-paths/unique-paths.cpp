7a1077bc71de91a14cad906d460a11e7a7bb2693
Kanhaiya Bagul <bagulkanhaiya60@gmail.com>
2025-12-17 21:21:48 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (49.82%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    
+    const long long MOD = 2 * 1e9;
+    int r , c;
+    int dfs(int i , int j,vector<vector<int>>& dp){
+        if(i >= r || j >= c) return 0;
+
+        if(i == r - 1 && j == c - 1){
+            return 1;
+        }
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        
+        int right = dfs(i,j+1,dp);
+        int down =  dfs(i+1,j,dp);
+
+        return dp[i][j] = (right + down);
+    }
+    int uniquePaths(int m, int n) {
+        r = m;
+        c = n;
+
+        vector<vector<int>> dp(m,vector<int> (n,-1));
+
+        return dfs(0,0,dp);
+    }
+};
