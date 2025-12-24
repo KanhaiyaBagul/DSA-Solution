891d875a2ae81ed68dd27fe6f450d5d1976b9835
Kanhaiya Bagul <bagulkanhaiya60@gmail.com>
2025-12-24 14:22:10 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (43.64%) - LeetSync

--- 1983-maximum-population-year/maximum-population-year.cpp
+++ 1983-maximum-population-year/maximum-population-year.cpp
@@ -2,19 +2,20 @@ class Solution {
 public:
     int maximumPopulation(vector<vector<int>>& logs) {
         map<int,int> log;
+        //first make a map , if any born increment 1 , any die decrement 1
         for(auto& it : logs){
             log[it[0]]++;
             log[it[1]]--;
         }        
 
-        int maxPopulation = INT_MIN;
-        int earlyYear = INT_MAX;
-        int population = 0;
+        int maxPopulation = INT_MIN;//for the max Population
+        int earlyYear = INT_MAX;//for the early year
+        int population = 0;//for the curr Population
         
         for(auto& it : log){
-            population += it.second;
+            population += it.second;//add the if it is born or dead
 
-            if(population > maxPopulation){
+            if(population > maxPopulation){//if we found the bigger population than maxPopulation , update the maxPopulation and also update the earlyYear
                 maxPopulation = population;
                 earlyYear = it.first;
             }
