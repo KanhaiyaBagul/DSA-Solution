f1d444e30a2a2749b0134a00903c7dd465e16dcc
Kanhaiya Bagul <bagulkanhaiya60@gmail.com>
2025-12-26 06:39:10 +0530

    Time: 7 ms (67.76%) | Memory: 13.4 MB (67.65%) - LeetSync

--- 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.cpp
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.cpp
@@ -6,22 +6,20 @@ public:
         int earlyHour = INT_MAX;
 
         int totalY = 0;
+        //find totalNumber of "N" in the string
         for(char ch : customers) if(ch == 'N') totalY++;
 
-        int N = 0;
-        int Y = 0;
+        int N = 0;//count the "N"
+        int Y = 0;//count the "Y"
 
         for (int i = 0; i <= customers.size(); i++) {
 
-            int penelty = N + (totalY - Y);
+            int penelty = N + (totalY - Y);//calculate the panelty , this is just the calculation of the "Y" and "N" for starting the the number of N = 0 caculate like 
+            //before the i th calculate how many number of "N" and after that aftert i th calculate the Number of "Y" this solve the problem
 
+            //this condition are use to before the i th calculate how many "N" and "Y" appear
             if(customers[i] == 'N') N++;
             else Y++;
-
-
-
-
-
 
             if (minResult > penelty) {
                 minResult = penelty;
