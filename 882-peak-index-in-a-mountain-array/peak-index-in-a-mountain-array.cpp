6e916da927d866113bccdfe2752d0d8e33725868
Kanhaiya Bagul <bagulkanhaiya60@gmail.com>
2026-01-11 22:53:35 +0530

    Time: 0 ms (100.00%) | Memory: 63.5 MB (77.53%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        int n = arr.size();
-        //In this problem out PEAK INDEX can't be the start and the end value of the arr so we start the starting index at 1 and the ending index at n - 2
-        int start = 1;
-        int end = n - 2;
+        int low = 1;
+        int high = arr.size() - 2;
 
-        while(start <= end){
-            int mid = start + (end - start) / 2;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+
             if(arr[mid - 1] < arr[mid] && arr[mid] > arr[mid + 1]){
                 return mid;
+            }else if(arr[mid] > arr[mid - 1] && arr[mid] < arr[mid+1]){
+                low = mid + 1;
+            }else{
+                high = mid - 1;
             }
-            else if(arr[mid - 1] < arr[mid]){
-                start = mid + 1;
-            }
-            else{
-                end = mid - 1;
-            }
-
-
         }
         return -1;
 
         
-        
-    } 
+    }
 };
