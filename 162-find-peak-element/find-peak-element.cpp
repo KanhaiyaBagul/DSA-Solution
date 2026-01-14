c91d819fa0cef63d16c5604012d9df2e0a5341dc
Kanhaiya Bagul <bagulkanhaiya60@gmail.com>
2026-01-14 10:50:58 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (83.61%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        
+
+        int low = 0;
+        int high = nums.size() - 1;
+
+        while(low < high){
+            int mid = low + (high - low) / 2;
+
+
+            if(nums[mid] < nums[mid + 1]){//if the solpe is in increasing the ans must lie in the right side
+                low = mid + 1;
+            }else{
+                high = mid;
+            }
+   
+        }
+        return low;
+        
+    }
+};
