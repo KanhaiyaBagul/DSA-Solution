79b3de9994e62f030cc97a4217dcab8d1ae1d94d
Kanhaiya Bagul <bagulkanhaiya60@gmail.com>
2026-01-24 20:25:44 +0530

    Time: 491 ms (57.27%) | Memory: 0B (100.00%) - LeetSync

--- 1161-project-employees-i/project-employees-i.sql
+++ 1161-project-employees-i/project-employees-i.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
+#first here we find the sum of all the experience we have and divide by the count of it
 select p.project_id , ROUND(sum(e.experience_years) / count(e.experience_years),2) as average_years
 from Project as p
 join Employee e
