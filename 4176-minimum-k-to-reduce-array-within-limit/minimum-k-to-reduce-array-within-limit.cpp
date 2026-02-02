182e141a6ff410284555aae3da57d0b369ad91e0
Kanhaiya Bagul <bagulkanhaiya60@gmail.com>
2026-02-02 10:16:44 +0530

    Time: 54 ms (94.44%) | Memory: 187.6 MB (5.56%) - LeetSync

---
+++ 4176-minimum-k-to-reduce-array-within-limit/minimum-k-to-reduce-array-within-limit.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    long long totalHits(vector<int>& nums, int k){
+        long long hit = 0;
+        for(int &num : nums){
+            hit += (num + k - 1) / k;
+        }
+        return hit;
+    }
+    int minimumK(vector<int>& nums) {
+
+        int low = 1;
+        int high = 100000;
+        int ans = high;
+
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+
+            if(totalHits(nums,mid) <= 1LL * mid * mid){
+                ans = mid;
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return ans;
+        
+    }
+};
