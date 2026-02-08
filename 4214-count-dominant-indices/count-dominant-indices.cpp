03148dcf4fbf558eb4b4eb57f0051dfb89179146
Kanhaiya Bagul <bagulkanhaiya60@gmail.com>
2026-02-08 15:54:06 +0530

    Time: 0 ms (100.00%) | Memory: 32.2 MB (76.93%) - LeetSync

---
+++ 4214-count-dominant-indices/count-dominant-indices.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int dominantIndices(vector<int>& nums) {
+        int n = nums.size();
+        int totalSum = accumulate(nums.begin(),nums.end(),0);
+        int count = 0;
+        int deno = n - 1;
+
+        for(int i = 0; i < n - 1; i++){
+            totalSum -= nums[i];
+
+            double avg = totalSum / deno;
+            deno--;
+            if(nums[i] > avg){
+                count++;
+            }
+
+        }
+        return count;
+        
+    }
+};
