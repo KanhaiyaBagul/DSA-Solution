1a515765aba162fee59e4bb7a1c74b7a01cbf19b
Kanhaiya Bagul <bagulkanhaiya60@gmail.com>
2026-02-13 12:52:55 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (47.63%) - LeetSync

---
+++ 2269-count-elements-with-strictly-smaller-and-greater-elements/count-elements-with-strictly-smaller-and-greater-elements.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countElements(vector<int>& nums) {
+        int mx = *max_element(nums.begin(),nums.end());
+        int mn = *min_element(nums.begin(),nums.end());
+        int count = 0;
+
+        for(int num : nums){
+            if(num > mn && num < mx){
+                count++;
+            }
+        }
+        return count;
+        
+    }
+};
