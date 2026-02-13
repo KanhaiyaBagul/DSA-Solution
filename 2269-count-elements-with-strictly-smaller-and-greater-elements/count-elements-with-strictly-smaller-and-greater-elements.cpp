11e522fe9b25d9d60d0882e7f792bde044e158c4
Kanhaiya Bagul <bagulkanhaiya60@gmail.com>
2026-02-13 12:53:13 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (78.11%) - LeetSync

--- 2269-count-elements-with-strictly-smaller-and-greater-elements/count-elements-with-strictly-smaller-and-greater-elements.cpp
+++ 2269-count-elements-with-strictly-smaller-and-greater-elements/count-elements-with-strictly-smaller-and-greater-elements.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int countElements(vector<int>& nums) {
+        //find the max and min element first 
         int mx = *max_element(nums.begin(),nums.end());
         int mn = *min_element(nums.begin(),nums.end());
         int count = 0;
