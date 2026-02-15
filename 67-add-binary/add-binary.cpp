2f3834fb067c9ba940a714c29b2b1aa2ccff2590
Kanhaiya Bagul <bagulkanhaiya60@gmail.com>
2026-02-15 11:11:23 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (80.99%) - LeetSync

--- 67-add-binary/add-binary.cpp
+++ 67-add-binary/add-binary.cpp
@@ -1,29 +1,34 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        string ans;
+
         reverse(a.begin(),a.end());
         reverse(b.begin(),b.end());
+        string ans = "";
         int carry = 0;
 
         for(int i = 0; i < max(a.size(),b.size()); i++){
-            
-            int digit1 = (i < a.size()) ? a[i] - '0' : 0;
-            int digit2 = (i < b.size() ? b[i] - '0' : 0);
-            
-            int total = digit1 + digit2 + carry;
-            ans.push_back(char(total % 2 + '0'));
+            int first = (i < a.size() ? a[i] - '0' : 0);
+            int second = (i < b.size() ? b[i] - '0' : 0);
 
-            carry = total /2;
-   
-         }
-         if(carry != 0){
+            int total = first + second + carry;
+
+
+            ans.push_back((total % 2) + '0' );
+
+            carry = total / 2;
+
+        }
+
+        if(carry != 0){
             ans.push_back('1');
+        }
 
-         }
-         reverse(ans.begin(),ans.end());
+        reverse(ans.begin(),ans.end());
+        return ans;
 
-         return ans;
+
+        
         
     }
 };
