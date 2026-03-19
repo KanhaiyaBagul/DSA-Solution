2af639c557b4ac3fe7ee3644206110f613d95ba4
Kanhaiya Bagul <bagulkanhaiya60@gmail.com>
2026-03-19 22:37:56 +0530

    Time: 0 ms (100.00%) | Memory: 28.8 MB (92.99%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.cpp
+++ 572-subtree-of-another-tree/subtree-of-another-tree.cpp
@@ -6,48 +6,46 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    bool helper(TreeNode* root, TreeNode* subRoot){
+    bool helper(TreeNode* root, TreeNode* subRoot) {
 
-        if(root == nullptr && subRoot == nullptr){
+        if (root == nullptr && subRoot == nullptr) {
             return true;
-        }else if(root == nullptr || subRoot == nullptr){
+        }
+        if (root == nullptr || subRoot == nullptr) {
             return false;
         }
 
-        if(root -> val != subRoot -> val){
+        if (root -> val!= subRoot -> val) {
             return false;
         }
 
-        bool left = helper(root -> left , subRoot -> left);
-        bool right = helper(root -> right , subRoot -> right);
+        bool left = helper(root->left, subRoot->left);
+        bool right = helper(root->right, subRoot->right);
+
         return left && right;
     }
-
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-
-        if(root  == nullptr && subRoot == nullptr){
+        if(root == nullptr && subRoot == nullptr){
             return true;
-        }else if(root == nullptr || subRoot == nullptr){
+        }if(root == nullptr || subRoot == nullptr){
             return false;
         }
 
-        if(root-> val == subRoot -> val){
-            if(helper(root, subRoot)){
+        if (root->val == subRoot->val) {
+            if (helper(root, subRoot)) {
                 return true;
             }
         }
 
-        bool left = isSubtree(root -> left, subRoot);
-        bool right = isSubtree(root -> right, subRoot);
+        bool left = isSubtree(root->left, subRoot);
+        bool right = isSubtree(root->right, subRoot);
 
         return left || right;
-
-        
-        
     }
 };
