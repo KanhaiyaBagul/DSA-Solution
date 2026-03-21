c1118de045cc954e5104c8573d18d368e1471301
Kanhaiya Bagul <bagulkanhaiya60@gmail.com>
2026-03-21 09:40:21 +0530

    Time: 0 ms (100.00%) | Memory: 34 MB (5.12%) - LeetSync

--- 3973-flip-square-submatrix-vertically/flip-square-submatrix-vertically.cpp
+++ 3973-flip-square-submatrix-vertically/flip-square-submatrix-vertically.cpp
@@ -5,8 +5,8 @@ public:
         int m = grid.size();
         int n = grid[0].size();
 
-        for(int i = 0; i < k / 2; i++){
-            for(int j = 0; j < k;j++){
+        for(int i = 0; i < k / 2; i++){//this handels the row 
+            for(int j = 0; j < k;j++){//this handels each element to swap in two rows
                 swap(grid[x+i][y+j],grid[x + (k-1-i)][y+j]);
             }
         }
